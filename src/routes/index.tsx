ac7f2cc1b0ba0e98ff53eb3e9a82f4896730ab18
Robson Souza <robsonex@gmail.com>
2020-05-04 20:30:32 -0400

    first commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Dashboard from '../pages/Dashboard';
+import Repositoy from '../pages/Repository';
+
+const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" exact component={Dashboard} />
+    <Route path="/repository" component={Repositoy} />
+  </Switch>
+);
+
+export default Routes;
