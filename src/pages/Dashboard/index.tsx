729ecaabb98632281303a98dde7ea52c54b008d5
Robson Souza <robsonex@gmail.com>
2020-05-05 13:43:33 -0400

    add logo and input

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { Title, Form } from './styles';
+
+import logoImg from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+    </>
+  );
 };
 
 export default Dashboard;
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -3,4 +3,23 @@ import styled from 'styled-components';
 export const Title = styled.h1`
   font-size: 48px;
   color: #3a3a3a;
+  max-width: 450px;
+  line-height: 56px;
+
+  margin-top: 80px;
+`;
+
+export const Form = styled.form`
+  margin-top: 40px;
+  max-width: 700;
+
+  display: flex;
+
+  input {
+    flex: 1;
+  }
+
+  button {
+    width: 210px;
+  }
 `;
